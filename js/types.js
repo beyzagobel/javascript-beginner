84c6cdebbb0c853f0f50e178ce581dbe8771b989
beyzagobel <byzgbll@gmail.com>
2022-08-11 11:40:38 +0300

    kosul.js commit

--- js/types.js
+++ js/types.js
@@ -2,3 +2,18 @@ let price = 100;
 let tax = 0.12;
 let priceTax = price*tax;
 console.log(price,tax,priceTax);
+
+
+
+
+let name = `beyza`
+console.log(`merhaba ben ${name}`)
+
+
+let title = "deneme";
+let title2 = document.getElementById(title);
+
+
+let link = document.querySelector("#link")
+
+console.log(link)
