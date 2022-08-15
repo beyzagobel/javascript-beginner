a2ad0bf76a5ce3cdc685b66f35a4af150f8394cb
beyzagobel <byzgbll@gmail.com>
2022-08-15 15:11:01 +0300

    new

--- js/09-string-data-types.js
+++ js/09-string-data-types.js
@@ -20,3 +20,44 @@ console.log(isim.slice(2));  // yza Gobel
 
 // replace : Aranan metni istenilen ile değiştirilir
 
+console.log(isim.replace("Gobel", "Göbel"));  // Beyza Göbel
+
+// toUpperCase, ToLowerCase
+
+console.log(isim.toUpperCase());  // BEYZA GOBEL
+console.log(isim.toLowerCase());  // beyza gobel
+
+// concat : metin birleştirme, iki string veri birleştirilir.
+
+var ad = "Beyza";
+var soyad = "Gobel";
+console.log(ad.concat(" ", soyad));   // Beyza Gobel
+
+
+// Split : Metni diziye çevirme , KUllanılan parametre  ile metni nasıl parçalanacağı belitilir.
+
+console.log(isim.split(" "));  // ['Beyza', 'Gobel']
+
+
+// String bir ifadenin içerisinde aradığımız karakterin yerini bulmak için 2 yöntem vardır. [] ve charAt()
+console.log(ad[0]); //"B"
+console.log(ad.charAt(2)); //"y"
+
+// ilk harf büyük diğer harfler küçük yapma
+var deneme = "DENEME";
+deneme = `${deneme[0].toUpperCase()}${deneme.slice(1).toLowerCase()}`;
+console.log(deneme);  // Deneme
+
+
+let url = "www.beyzagobel.com";
+let language = "Java";
+
+ // language değişkeninin değerini JavaScript olarak değiştiriğ konsola yazın
+ language = language.replace("Java","JavaScript");
+ console.log(language);
+
+ //url değişkeninin www olmadan domain(beyzagobel.com) adında yeni değişken oluşturun ve kosola yazdırın
+ 
+let nokta = url.indexOf(".");
+let domain = url.slice(nokta+1);
+console.log(domain);   // beyzagobel.com
